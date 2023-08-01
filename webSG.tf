58a9bf719a52452d8d0bfc226dbea9e936732c36
vishnu <vichu_77@yahoo.com>
2023-08-01 21:49:17 +0530

    Final Commit on 8/1

--- webSG.tf
+++ webSG.tf
@@ -1,3 +1,9 @@
+variable "lb_sg_ports" {
+  type        = list(number)
+  description = "list of ingress ports"
+  default     = [80, 443]
+}
+
 variable "web_sg_ports" {
   type        = list(number)
   description = "list of ingress ports"
@@ -10,12 +16,60 @@ variable "db_sg_ports" {
   default     = [80, 443]
 }
 
-resource "aws_security_group" "dynamicsg" {
-  name        = "web-hosts-sg"
-  description = "Ingress from LB"
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  description = "Ingress from Internet"
+
+  dynamic "ingress" {
+    for_each = var.lb_sg_ports
+    content {
+      from_port   = 0
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "egress" {
+    for_each = var.lb_sg_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "lb-sg"
+  description = "Ingress from Internet"
 
   dynamic "ingress" {
     for_each = var.web_sg_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  dynamic "egress" {
+    for_each = var.web_sg_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "lb-sg"
+  description = "Ingress from Internet"
+
+  dynamic "ingress" {
+    for_each = var.db_sg_ports
     content {
       from_port   = ingress.value
       to_port     = ingress.value
